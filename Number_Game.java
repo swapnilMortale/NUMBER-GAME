a894d21b7d06f27ad2cc91001fe35cec96fb5473
MORTALE SWAPNIL <157574425+swapnilMortale@users.noreply.github.com>
2024-07-12 17:40:23 +0530

    Add files via upload

---
+++ Number_Game.java
@@ -0,0 +1,28 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package number_game;
+
+/**
+ *
+ * @author swapn
+ */
+public class Number_Game {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        new Homepage();
+    }
+    
+}
+
+
+
+
+
+//@SWAPNIL MORTALE
